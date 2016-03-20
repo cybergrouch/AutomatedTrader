173cc8ac2efdd44dcd078cf536e96c4e2dc2c12d
cybergrouch <cyber_grouch_lange@yahoo.ie>
2016-03-20 10:22:35 +0800

    Clean up Repl.

--- src/main/java/com/lange/trader/main/Repl.java
+++ src/main/java/com/lange/trader/main/Repl.java
@@ -13,8 +13,6 @@ import com.lange.trader.struc.Pair;
 
 import java.io.*;
 import java.util.*;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -279,51 +277,6 @@ public class Repl {
                                 .collect(Collectors.toList()));
             }
             return Optional.of(Pair.of(command.get(), arguments));
-        }
-    }
-
-    static class ZippingSpliterator<L, R, O> implements Spliterator<O> {
-
-        static <L, R, O> Spliterator<O> zipping(Spliterator<L> lefts, Spliterator<R> rights, BiFunction<L, R, O> combiner) {
-            return new ZippingSpliterator<>(lefts, rights, combiner);
-        }
-
-        private final Spliterator<L> lefts;
-        private final Spliterator<R> rights;
-        private final BiFunction<L, R, O> combiner;
-        private boolean rightHadNext = false;
-
-        private ZippingSpliterator(Spliterator<L> lefts, Spliterator<R> rights, BiFunction<L, R, O> combiner) {
-            this.lefts = lefts;
-            this.rights = rights;
-            this.combiner = combiner;
-        }
-
-        @Override
-        public boolean tryAdvance(Consumer<? super O> action) {
-            rightHadNext = false;
-            boolean leftHadNext = lefts.tryAdvance(l ->
-                    rights.tryAdvance(r -> {
-                        rightHadNext = true;
-                        action.accept(combiner.apply(l, r));
-                    }));
-            return leftHadNext && rightHadNext;
-        }
-
-        @Override
-        public Spliterator<O> trySplit() {
-            return null;
-        }
-
-        @Override
-        public long estimateSize() {
-            return Math.min(lefts.estimateSize(), rights.estimateSize());
-        }
-
-        @Override
-        public int characteristics() {
-            return lefts.characteristics() & rights.characteristics()
-                    & ~(Spliterator.DISTINCT | Spliterator.SORTED);
         }
     }
 }
